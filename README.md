801ff64dd83b65d73cab7d40a3a68230f07d7870
Matthias Bruns <matthias.bruns@dg-i.net>
2016-09-03 10:52:31 +0200

    added scss folder structure and README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Webpack Base Biolerplate
+
+This projects aims to simplify the frontend development using webpack.
+It handles basic things like scss compilation, js bundling and development hot reloading.
+
+## Getting Started
+
+````npm run dev````
+Run development server
+
+````npm run build````
+Build for production
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 // Since Babel only transforms syntax (like arrow functions), you can use babel-polyfill in order to support new globals such as Promise or new native methods like String.padStart (left-pad).
 import 'babel-polyfill'
 // SCSS reference - will be compiles by webpack
-import './styles/base.scss'
+import './stylesheets/base.scss'
